1703a65fe3cc0696873f80d4e67c5721929a1751
Danyil <dartomov@gmail.com>
2024-10-10 15:40:38 +0200

    Create paths for viewsets with routers and include them in urlpatterns

---
+++ planetarium/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import (
+    AstronomyShowViewSet,
+    AstronomyThemeViewSet,
+    ShowSessionViewSet,
+    PlanetariumDomeViewSet,
+    ReservationViewSet,
+    ShowThemeViewSet,
+    TicketViewSet,
+)
+
+
+router = routers.DefaultRouter()
+
+router.register("show-sessions", ShowSessionViewSet)
+router.register("planetarium-domes", PlanetariumDomeViewSet)
+router.register("astronomy-shows", AstronomyShowViewSet)
+router.register("show-themes", ShowThemeViewSet)
+router.register("astronomy-themes", AstronomyThemeViewSet)
+router.register("tickets", TicketViewSet)
+router.register("reservations", ReservationViewSet)
+
+app_name = "planetarium"
+
+urlpatterns = [
+    path("", include(router.urls))
+]
