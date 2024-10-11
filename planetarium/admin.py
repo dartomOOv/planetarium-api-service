cfc1fe8957657fe20245c64d3d1b403546414b66
Danyil <dartomov@gmail.com>
2024-10-11 08:09:22 +0200

    Register planetarium models in admin site

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (
+    AstronomyShow,
+    AstronomyTheme,
+    Reservation,
+    PlanetariumDome,
+    ShowSession,
+    ShowTheme,
+    Ticket,
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInline]
+
+
+admin.site.register(ShowSession)
+admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyTheme)
+admin.site.register(Ticket)
