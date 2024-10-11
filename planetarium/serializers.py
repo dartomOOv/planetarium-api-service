a017132d04fa50d9bd89c2f63eb8688dd595d3f3
Danyil <dartomov@gmail.com>
2024-10-11 08:26:30 +0200

    Create list and detail serializers for ShowSession model

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -4,12 +4,6 @@ from planetarium.models import ShowSession, PlanetariumDome, AstronomyShow, Show
     Reservation
 
 
-class ShowSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShowSession
-        fields = "__all__"
-
-
 class PlanetariumDomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanetariumDome
@@ -32,6 +26,22 @@ class AstronomyThemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AstronomyTheme
         fields = "__all__"
+
+
+class ShowSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShowSession
+        fields = ["id", "astronomy_show", "planetarium_dome", "show_time"]
+
+
+class ShowSessionListSerializer(ShowSessionSerializer):
+    astronomy_show = serializers.SlugRelatedField(slug_field="title", read_only=True)
+    planetarium_dome = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+
+class ShowSessionDetailSerializer(ShowSessionSerializer):
+    astronomy_show = AstronomyShowSerializer()
+    planetarium_dome = PlanetariumDomeSerializer()
 
 
 class TicketSerializer(serializers.ModelSerializer):
