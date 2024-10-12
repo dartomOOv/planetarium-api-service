3cb198c6caa10279acd4c3a6d5bc73483eb70532
Danyil <dartomov@gmail.com>
2024-10-12 14:41:01 +0200

    Add INTERNAL_IPS in config setting

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
