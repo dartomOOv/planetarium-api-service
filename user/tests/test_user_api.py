c983246026a335d2b3075be112b99a57550600b6
Danyil <dartomov@gmail.com>
2024-10-15 14:38:16 +0200

    Add tests for user views.

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -5,12 +5,13 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
+from config.settings import AUTH_USER_MODEL
 
 CREATE_USER_URL = reverse("user:create")
 ME_URL = reverse("user:manage")
 
 
-def create_user(**params):
+def create_user(**params) -> AUTH_USER_MODEL:
     return get_user_model().objects.create_user(**params)
 
 
@@ -49,9 +50,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = (
-            get_user_model().objects.filter(email=payload["email"]).exists()
-        )
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
         self.assertFalse(user_exists)
 
 
