0d504747ad191b4a483f1e3785aedba9c131d1a4
Danyil <dartomov@gmail.com>
2024-10-17 11:08:31 +0200

    Remove reduntant permission class in ManageUserView class view.

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
 
@@ -12,7 +11,6 @@ class UserCreateView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
