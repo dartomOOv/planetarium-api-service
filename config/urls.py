2733309cdeedf4f43154cd8c6367e2f47153aa83
Danyil <dartomov@gmail.com>
2024-10-17 10:57:03 +0200

    Fix url naming in config urls.

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/users/", include("user.urls", namespace="user")),
     path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
